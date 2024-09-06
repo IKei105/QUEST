841fd2f2111896c184dd4b9ea910a27da555a997
IKei105 <natsuki.kei105@gmail.com>
2024-09-06 11:14:11 +0900

    ステップ3のメールアドレスを入力、ファイルの複合化、暗号化を記述

--- password_manager.sh
+++ password_manager.sh
@@ -5,6 +5,8 @@ readonly USER_NAME=1
 readonly PASSWORD=2
 
 echo "パスワードマネージャーへようこそ！"
+echo "鍵として登録したメールアドレスを入力してください。"
+read mail_address
 while true
 do
   echo "次の選択肢から入力してください(Add Password/Get Password/Exit)："
@@ -18,7 +20,18 @@ do
     read password
 
     password_info="${service_name}:${user_name}:${password}"
+    #ここでパスワード情報の複合化を行う、複合化したものをpassword_info.txtに格納する
+    #ここは暗号化ファイルが存在した場合、複合を行う
+    gpg -d password_info.txt.gpg > password_info.txt
     echo $password_info >> password_info.txt
+    echo $password_info >> text.txt
+    gpg -r $mail_address -e password_info.txt 
+      if [ $? -eq 0 ]; then
+	rm password_info.txt
+      else
+	echo "エラーが発生しました。終了します。"	
+      fi
+    echo ${encrypt_result}
     echo "パスワードの追加は成功しました。"
   elif [ "${input}" = "Get Password" ]; then
     echo -n "サービス名を入力してください："
